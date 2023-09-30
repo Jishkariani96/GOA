e1818c5b83e0dd05f6abb0bc337eddd80d6926a1
Guri <123614932+Jishkariani96@users.noreply.github.com>
2023-09-30 20:14:48 +0400

    Add files via upload

---
+++ day3/homework/homework.py
@@ -0,0 +1,9 @@
+
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+age = str(input("Enter your age: "))
+
+print("my name is " + name + " " + surname + " and I am " + age + " years old")
+
+
+
---
+++ day3/homework/homework2.py
@@ -0,0 +1,4 @@
+
+user_age = int(input("enter user's age: "))
+
+print("After 25 years you'll be " + str(user_age + 25) + " years old.")
