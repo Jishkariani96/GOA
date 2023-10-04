577c3ad11f6044874edf67925c18f56fdd3dcee4
Guri <123614932+Jishkariani96@users.noreply.github.com>
2023-10-04 21:20:35 +0400

    Add files via upload

---
+++ day4/homework/homework.py
@@ -0,0 +1,10 @@
+
+age = int(input("Enter your age: "))
+print(((age * 3) // 7 ) + (4 * 9))
+
+
+
+
+
+
+
---
+++ day4/homework/homework2.py
@@ -0,0 +1,3 @@
+car_price = 7000
+new_price = 7000 + (7000//100 * 62)
+print("Car became more expensive by " + str(new_price - car_price) + " dollars")
