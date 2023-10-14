ea38a940863efbfe4ec8fd3c97e0a480f5dd6c71
Guri <123614932+Jishkariani96@users.noreply.github.com>
2023-10-14 20:49:49 +0400

    Add files via upload

---
+++ day7/homework/homework.py
@@ -0,0 +1,3 @@
+goa_squad_leaders = ["Dato Janezashvili", "Gabriel Molodini","Davit Tkeshelashvili", "Nika Filishvili"]
+my_leader = goa_squad_leaders[1]
+print(my_leader)
---
+++ day7/homework/homework3.py
@@ -0,0 +1,7 @@
+fruit_list = ["Apple", "Banana", "Pear", "Cucumber", "Cocoa"]
+fruit_list[3] = "Orange"
+print(fruit_list)
+
+
+
+
